7a714600f5c6f0857f661310f6458cfad788f420
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2018-09-28 12:30:39 +0200

    Update test data

--- api/wifi-ap-results.js
+++ api/wifi-ap-results.js
@@ -1,6 +1,6 @@
 exports.wifiAP = [
     { ssid: 'TN_24GHz_62FAE1', saved: true, status: 'connected' },
     { ssid: 'kattnet', saved: true },
-    { ssid: 'new-wifi', saved: true },
     { ssid: 'my-wifi', saved: true },
+    { ssid: 'new-wifi', saved: true },
 ];

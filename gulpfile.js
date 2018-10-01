f5497cac358af792976553d8408a94a1759a772b
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2018-10-01 17:40:04 +0200

    Make eslint happy

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,30 @@ const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify-es').default;
 const brotli = require('gulp-brotli');
-const zopfli = require("gulp-zopfli-green");
+const zopfli = require('gulp-zopfli-green');
 const clean = require('gulp-clean');
 
-gulp.task('clean', () => {
-    return gulp.src('dist/', { read: false })
-        .pipe(clean());
-});
+gulp.task('clean', () => gulp.src('dist/', { read: false })
+    .pipe(clean()));
 
-gulp.task('build', () => {
-    return gulp.src('src/index.html')
-        .pipe(inline({
-            base: 'src/',
-            js: uglify,
-            css: cleanCSS,
-        }))
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('dist/'));
-});
+gulp.task('build', () => gulp.src('src/index.html')
+    .pipe(inline({
+        base: 'src/',
+        js: uglify,
+        css: cleanCSS,
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/')));
 
-gulp.task('favicon', () => {
-    return gulp.src('src/favicon.ico')
-        .pipe(gulp.dest('dist/'));
-});
+gulp.task('favicon', () => gulp.src('src/favicon.ico')
+    .pipe(gulp.dest('dist/')));
 
-gulp.task('brotli', () => {
-    return gulp.src(['dist/*', '!dist/*.gz', '!dist/*.br'])
-        .pipe(brotli.compress())
-        .pipe(gulp.dest('dist/'));
-});
+gulp.task('brotli', () => gulp.src(['dist/*', '!dist/*.gz', '!dist/*.br'])
+    .pipe(brotli.compress())
+    .pipe(gulp.dest('dist/')));
 
-gulp.task('zopfli', () => {
-    return gulp.src(['dist/*', '!dist/*.gz', '!dist/*.br'])
-        .pipe(zopfli({ format: 'gzip' }))
-        .pipe(gulp.dest('dist/'));
-});
+gulp.task('zopfli', () => gulp.src(['dist/*', '!dist/*.gz', '!dist/*.br'])
+    .pipe(zopfli({ format: 'gzip' }))
+    .pipe(gulp.dest('dist/')));
 
 gulp.task('default', gulp.series('clean', 'build', 'favicon', 'zopfli', 'brotli'));

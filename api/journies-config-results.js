fbf7ac9f2adaabc61c4bc31924edea39058de26d
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2018-12-29 10:57:11 +0100

    Correct spelling of json field

--- api/journies-config-results.js
+++ api/journies-config-results.js
@@ -1,8 +1,8 @@
 exports.journies = [
     {
-        line: '80', stop: 'Saltsjöqvarn', destination: 'Nybroplan', site_id: 1442, mode: 5, direction: 2,
+        line: '80', stop: 'Saltsjöqvarn', destination: 'Nybroplan', 'site-id': 1442, mode: 5, direction: 2,
     },
     {
-        line: '53', stop: 'Henriksdalsberget', destination: 'Karolinska institutet', site_id: 1450, mode: 1, direction: 2,
+        line: '53', stop: 'Henriksdalsberget', destination: 'Karolinska institutet', 'site-id': 1450, mode: 1, direction: 2,
     },
 ];
